4b6b30d79e55da391eed1dc077370f764c5ffb52
ras1k <rasikabdullah@gmail.com>
2023-04-16 01:34:07 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className='container'>JSX</h1>
+        <div className="music">
+          <p>Name : </p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
